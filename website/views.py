ba87bca236273d7186cbd0a4f9d7aeb964fa8081
Nezerac-Final-God-of-Pain <jamietindle07@gmail.com>
2025-10-16 12:13:27 +0100

    Add files via upload

--- website/views.py
+++ website/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from .forms import CreateUserForm, LoginForm
 
 def home(request):
     return render(request, 'website/index.html')
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #return redirect('')
+
+    context = {'form': form}
+
+    return render(request,'website/register.html', context=context)
